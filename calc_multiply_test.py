a794577c54859e7cbbf572837d57ec825196a73b
Eric <eric.hibbard91@gmail.com>
2024-11-25 13:25:15 -0700

    fourth commit

---
+++ calc_multiply_test.py
@@ -0,0 +1,8 @@
+def test_multiply():
+    a = 2
+    # Define the second number
+    b = 3
+    # Perform the multiplication operation
+    multiply = a * b
+    # Check if the user gets the expected
+    assert multiply == 6
---
+++ calc_test.py
@@ -0,0 +1,9 @@
+# This test calculates the total sum and checks if the test has passed or failed.
+def test_sum_1():
+    assert 2 + 2 == 4
+
+def test_sum_2():
+    assert 3 + 1 == 4
+
+def test_sum_3():
+    assert 5 - 1 == 4
